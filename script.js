d8635091e0eecf1501463efdf5fb0ac890d8026a
Osei Okyere <okyereosei@gmail.com>
2023-07-21 21:43:11 -0500

    script js

---
+++ script.js
@@ -0,0 +1,8 @@
+const num = 30;
+// if( num < 5 ){
+//     console.log('Num is less than 5')
+// } else {
+//     console.log('Num is greater than 5')
+// }
+
+num<5 ? console.log('Num is less than 5') : console.log('Num is greater than 5');
